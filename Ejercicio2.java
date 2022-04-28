7910bf3a9e4968883bf503995c3e2dc2f241df51
Ivan Zheng <zhengivan10@gmail.com>
2022-04-28 20:16:16 +0200

    Ejercicio 5 hecho

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -9,34 +9,25 @@ public class Ejercicio2 {
 
 	public static void main(String[] args) {
 		String codigo = "Null";
-		
-		ArrayList <String> paises = new ArrayList<String>();
-			String eeuucan = ("^[00-13]{2,3}$");
-				paises.add(eeuucan);
-			String alemania = ("^[40-44]{2,3}$");
-				paises.add(alemania);
-			String Taiwan = ("^[20.29]{2,3}$");
-				paises.add(Taiwan);
-			String Francia = ("^[30-37]{2,3}$");
-				paises.add(Francia);
-			String demas = (".*");
-				paises.add(demas);
-		
-		Pattern p = Pattern.compile(codigo);
-		Matcher m= p.matcher(codigo);
-		
-		for (int i = 0; i < paises.size(); i++) {
-			String aux= paises.get(i);
-			
-			switch (codigo.matches(aux)) {
-			case value:
-				
-				break;
 
-			default:
-				break;
-			}
+		ArrayList<String> paises = new ArrayList<String>();
+		String eeuucan = ("^[00-13]{2,3}$");
+		paises.add(eeuucan);
+		String alemania = ("^[40-44]{2,3}$");
+		paises.add(alemania);
+		String Taiwan = ("^[20.29]{2,3}$");
+		paises.add(Taiwan);
+		String Francia = ("^[30-37]{2,3}$");
+		paises.add(Francia);
+		String demas = (".*");
+		paises.add(demas);
+
+		Pattern p = Pattern.compile(codigo);
+		Matcher m = p.matcher(codigo);
+
+		for (int i = 0; i < paises.size(); i++) {
+			String aux = paises.get(i);
 		}
-		
+
 	}
 }
--- Ejercicio5.java
+++ Ejercicio5.java
@@ -5,13 +5,16 @@ import java.util.regex.Pattern;
 
 public class Ejercicio5 {
 	public static void main(String[] args) {
+
 		String ip = "192.168.1.1";
-	
-	Pattern p = Pattern.compile("^([01]?[0-9](1,2)|2[0-4][0-9]|25[0-5])\\.([01]?[0-9](1,2)|2[0-4][0-9]|25[0-5])\\\\.([01]?[0-9](1,2)|2[0-4][0-9]|25[0-5])\\\\.([01]?[0-9](1,2)|2[0-4][0-9]|25[0-5])\\\\$");
-	Matcher m= p.matcher(ip);
-	boolean validarIp = m.matches();
-	if (validarIp == true) {
-		System.out.println("Es una IP");
+
+		Pattern p = Pattern.compile("^((25[0-5]|2[0-4]\\d|[01]?\\d\\d?)\\.){3}(25[0-5]|2[0-4]\\d|[01]?\\d\\d?)$");
+		Matcher m = p.matcher(ip);
+
+		boolean validarIp = m.matches();
+
+		if (validarIp == true) {
+			System.out.println("Es una IP");
 		} else {
 			System.out.println("No es una IP");
 		}
